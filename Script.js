fcbcbcfaf649d396fbd38920c2fdf42f630948af
Tony Harris <tony_harris25@yahoo.com>
2016-11-13 21:47:24 -0500

    Clears input, image, and response when button is clicked and began working on structure for determining the response when the user asks a question

--- Script.js
+++ Script.js
@@ -1,3 +1,40 @@
 /**
  * Created by TonyHarris on 10/27/16.
  */
+const yes = 0;
+const no = 1;
+const maybe = 2;
+const askAgain = 3;
+
+function RandomInt(low,high){
+    return Math.floor(Math.random()*(high-low+1)) + low;
+}
+
+function $(element){
+
+return document.getElementById(element);
+}
+
+function ClearForm(){
+    $('txtUserData').value='';
+    $('div8BallImage').src='';
+    $('div8BallResponse').value='';
+}
+
+function EightBall(){
+    ClearForm();
+
+    var Choice;
+    if (RandomInt = yes){
+    $('div8BallResponse').value = 'Yes'
+    }
+    else if (RandomInt = no) {
+        $('div8BallResponse').value = 'No'
+    }
+    else if (RandomInt = maybe) {
+        $('div8BallResponse').value = 'Maybe'
+    }
+    else if (RandomInt = askAgain) {
+        $('div8BallResponse').value = 'Ask Again'
+    }
+}
