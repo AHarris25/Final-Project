aff5401ebe9a38a4626807124264ca3c98434558
Tony Harris <tony_harris25@yahoo.com>
2016-11-28 20:01:36 -0500

    I've got a lot of great ideas like adding an alert if they don't type something in and adding an image to a response but I just can't figure anything out.

--- Script.js
+++ Script.js
@@ -1,14 +1,16 @@
 /**
  * Created by TonyHarris on 10/27/16.
  */
-var response = ['Maybe.','Not in your wildest dreams.',
+var dreams = 'images/dreams.png';
+
+var answers = ['Maybe.','Not in your wildest dreams.',
     'There is a good chance.', 'Never!', 'Hell, yes.', 'Hell no.',
     'The future is uncertain.', 'Possibly.','Yes!'];
 
-function Random(response){
-    return response[Math.floor(Math.random()*response.length)];
+function Random(){
+    var answer = answers[Math.floor(Math.random() * answers.length)];
+    return answer;
 }
-
 function $(element){
     return document.getElementById(element);
 }
@@ -20,34 +22,39 @@ function ClearForm(){
 }
 
 function EightBall(){
-    var Answer = Random(response);
+    if ($("txtUserData") == "" ){
+        $("divError").innerHTML = "* No Question Entered";
+        return;
+    }
+    var Answer = Random(answers);
     var Image = '';
-    if (Answer = 'Maybe'){
-        $('div8BallImage').src = 'images/Maybe.png';
+    if (Answer == 'Maybe'){
+        Image = 'images/Maybe.png';
     }
-    else if (Answer = 'Not in your wildest dreams.'){
-        $('div8BallImage').src = 'images/dreams.png';
+    else if (Answer == 'Not in your wildest dreams.'){
+        Image = dreams  ;
     }
-    else if (Answer = 'There is a good chance.'){
-        $('div8BallImage').src = 'images/Good chance.png';
+    else if (Answer == 'There is a good chance.'){
+        Image = 'images/Good chance.png';
     }
-    else if (Answer = 'Never!'){
-        $('div8BallImage').src = 'images/Never.png';
+    else if (Answer == 'Never!'){
+        Image = 'images/Never.png';
     }
-    else if (Answer = 'Hell, yes.'){
-        $('div8BallImage').src = 'images/Hell yes.png';
+    else if (Answer == 'Hell, yes.'){
+        Image = 'images/Hell yes.png';
     }
-    else if (Answer = 'Hell no.'){
-        $('div8BallImage').src = 'images/Hell no.png';
+    else if (Answer == 'Hell no.'){
+        Image = 'images/Hell no.png';
     }
-    else if (Answer = 'The future is uncertain.'){
-        $('div8BallImage').src = 'images/the future is uncertain.png';
+    else if (Answer == 'The future is uncertain.'){
+        Image = 'images/the future is uncertain.png';
     }
-    else if (Answer = 'Possibly.'){
-        $('div8BallImage').src = 'images/possibly.png';
+    else if (Answer == 'Possibly.'){
+        Image = 'images/possibly.png';
     }
-    else if (Answer = 'Yes!'){
-        $('div8BallImage').src = 'images/Yes.png';
+    else if (Answer == 'Yes!'){
+        Image = 'images/Yes.png';
     }
     $('div8BallResponse').innerHTML = Answer;
+    $('div8BallImage').src = Image;
 }
