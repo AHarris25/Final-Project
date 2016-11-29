b559ba44bcd76abbb37393bca748c7c0973f3a29
Tony Harris <tony_harris25@yahoo.com>
2016-11-28 19:25:30 -0500

    Changed the structure of getting a random response a few time but forgot to make the commits...

--- Script.js
+++ Script.js
@@ -1,8 +1,7 @@
 /**
  * Created by TonyHarris on 10/27/16.
  */
-var response = [
-    'Maybe.','Not in your wildest dreams.',
+var response = ['Maybe.','Not in your wildest dreams.',
     'There is a good chance.', 'Never!', 'Hell, yes.', 'Hell no.',
     'The future is uncertain.', 'Possibly.','Yes!'];
 
@@ -11,7 +10,7 @@ function Random(response){
 }
 
 function $(element){
-return document.getElementById(element);
+    return document.getElementById(element);
 }
 
 function ClearForm(){
@@ -22,5 +21,33 @@ function ClearForm(){
 
 function EightBall(){
     var Answer = Random(response);
+    var Image = '';
+    if (Answer = 'Maybe'){
+        $('div8BallImage').src = 'images/Maybe.png';
+    }
+    else if (Answer = 'Not in your wildest dreams.'){
+        $('div8BallImage').src = 'images/dreams.png';
+    }
+    else if (Answer = 'There is a good chance.'){
+        $('div8BallImage').src = 'images/Good chance.png';
+    }
+    else if (Answer = 'Never!'){
+        $('div8BallImage').src = 'images/Never.png';
+    }
+    else if (Answer = 'Hell, yes.'){
+        $('div8BallImage').src = 'images/Hell yes.png';
+    }
+    else if (Answer = 'Hell no.'){
+        $('div8BallImage').src = 'images/Hell no.png';
+    }
+    else if (Answer = 'The future is uncertain.'){
+        $('div8BallImage').src = 'images/the future is uncertain.png';
+    }
+    else if (Answer = 'Possibly.'){
+        $('div8BallImage').src = 'images/possibly.png';
+    }
+    else if (Answer = 'Yes!'){
+        $('div8BallImage').src = 'images/Yes.png';
+    }
     $('div8BallResponse').innerHTML = Answer;
 }
